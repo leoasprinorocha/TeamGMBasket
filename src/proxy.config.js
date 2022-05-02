e745b2e488d6ed08c24a0abeae46d92c0951f75a
LeonardoAsprinoRocha <asprinoufmscg@outlook.com>
2022-05-01 22:54:06 -0400

    Correção liberação link externo

--- src/proxy.config.js
+++ src/proxy.config.js
@@ -1,7 +1,7 @@
 const proxy = [
   {
   context: '/api',
-  target:'http://192.168.100.19:46000',
+  target:['http://192.168.100.19:46000','http://gerenciaapi-001-site1.itempurl.com'],
   pathRewrite: { '^/api': '' }
   }
   ];
